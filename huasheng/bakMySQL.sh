f7c7cdccd8070a4d2e8b1b891a195f0d499972d7
root <root@localhost.localdomain>
2014-02-17 05:26:10 +0800

    add cp

--- huasheng/bakMySQL.sh
+++ huasheng/bakMySQL.sh
@@ -2,5 +2,7 @@
 ##################
 # bak MySQL db   #
 ##################
+# 1. cp origin huasheng.sql
+cp huasheng.sql huasheng.sql.bak
 mysqldump --socket=/opt/mysql/tmp/mysql.sock -c -uroot -proot123 --default-character-set=utf8 huasheng > huasheng.sql
 
