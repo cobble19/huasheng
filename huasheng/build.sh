85b442a30a2f4d4128c1d07aabfc29e612623ffc
root <root@localhost.localdomain>
2014-03-20 22:00:15 +0800

    modify build.sh

--- huasheng/build.sh
+++ huasheng/build.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 . /etc/profile
+HS_BASE=/opt/app/huasheng/huasheng
 CATALINA_HOME=`echo ${CATALINA_HOME}`
 echo "Stop tomcat..."
 sh ${CATALINA_HOME}/bin/shutdown.sh
 # bak upload dir
 echo "Bak [upload] directory..."
-/bin/cp -R target/huasheng/upload/* temp/upload/
+/bin/cp -R ${HS_BASE}/target/huasheng/upload/* ${HS_BASE}/temp/upload/
 
+echo "cd ${HS_BASE} ..."
+cd ${HS_BASE}
 echo "Start git pull..."
-git pull
+git pull -q 
 echo "Start mvn..."
 mvn clean install -Dmaven.test.skip=true
 # Restore upload dir
 echo "Restore [upload] directory..."
-mkdir -p target/huasheng/upload
-/bin/cp -R temp/upload/* target/huasheng/upload/
+mkdir -p ${HS_BASE}/target/huasheng/upload
+/bin/cp -R ${HS_BASE}/temp/upload/* ${HS_BASE}/target/huasheng/upload/
 echo "Start tomcat..."
 sh ${CATALINA_HOME}/bin/startup.sh
 echo "Build finished..."
